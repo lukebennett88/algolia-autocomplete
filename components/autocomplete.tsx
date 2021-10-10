810dda6c086815cc588e974e766ac7495ff5e51b
Luke Bennett <luke.bennett@brighte.com.au>
2021-10-10 17:54:54 +1100

    Style autocomplete

--- components/autocomplete.tsx
+++ components/autocomplete.tsx
@@ -1,114 +1,232 @@
-import "@algolia/autocomplete-theme-classic";
-import "@algolia/autocomplete-theme-classic";
-
-import type { AutocompleteComponents } from "@algolia/autocomplete-js";
-import { autocomplete, getAlgoliaResults } from "@algolia/autocomplete-js";
+import {
+  AutocompleteOptions,
+  AutocompleteState,
+  createAutocomplete,
+} from "@algolia/autocomplete-core";
+import { getAlgoliaResults } from "@algolia/autocomplete-preset-algolia";
 import type { Hit } from "@algolia/client-search";
+import { ChevronRightIcon, SearchIcon, XIcon } from "@heroicons/react/outline";
 import algoliasearch from "algoliasearch/lite";
 import * as React from "react";
-import { render } from "react-dom";
+
+import { classNames } from "../utils";
 
 const searchClient = algoliasearch(
   "latency",
   "6be0576ff61c053d5f9a3225e2a90f76"
 );
 
-interface ProductItemProps {
-  hit: Hit<any>;
-  components: AutocompleteComponents;
-}
+type AutocompleteItem = Hit<{
+  brand: string;
+  categories: string[];
+  image: string;
+  name: string;
+  objectID: string;
+  url: string;
+}>;
 
-function ProductItem({ hit, components }: ProductItemProps) {
-  return (
-    <div className="aa-ItemWrapper">
-      <div className="aa-ItemContent">
-        <div className="aa-ItemIcon aa-ItemIcon--alignTop">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={hit.image} alt={hit.name} width="40" height="40" />
-        </div>
-        <div className="aa-ItemContentBody">
-          <div className="aa-ItemContentTitle">
-            <components.Snippet hit={hit} attribute="name" />
-          </div>
-          <div className="aa-ItemContentDescription">
-            <components.Snippet hit={hit} attribute="description" />
-          </div>
-        </div>
-        <div className="aa-ItemActions">
-          <button
-            className="aa-ItemActionButton aa-DesktopOnly aa-ActiveOnly"
-            type="button"
-            title="Select"
-          >
-            <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
-              <path d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z" />
-            </svg>
-          </button>
-          <button
-            className="aa-ItemActionButton"
-            type="button"
-            title="Add to cart"
-          >
-            <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
-              <path d="M19 5h-14l1.5-2h11zM21.794 5.392l-2.994-3.992c-0.196-0.261-0.494-0.399-0.8-0.4h-12c-0.326 0-0.616 0.156-0.8 0.4l-2.994 3.992c-0.043 0.056-0.081 0.117-0.111 0.182-0.065 0.137-0.096 0.283-0.095 0.426v14c0 0.828 0.337 1.58 0.879 2.121s1.293 0.879 2.121 0.879h14c0.828 0 1.58-0.337 2.121-0.879s0.879-1.293 0.879-2.121v-14c0-0.219-0.071-0.422-0.189-0.585-0.004-0.005-0.007-0.010-0.011-0.015zM4 7h16v13c0 0.276-0.111 0.525-0.293 0.707s-0.431 0.293-0.707 0.293h-14c-0.276 0-0.525-0.111-0.707-0.293s-0.293-0.431-0.293-0.707zM15 10c0 0.829-0.335 1.577-0.879 2.121s-1.292 0.879-2.121 0.879-1.577-0.335-2.121-0.879-0.879-1.292-0.879-2.121c0-0.552-0.448-1-1-1s-1 0.448-1 1c0 1.38 0.561 2.632 1.464 3.536s2.156 1.464 3.536 1.464 2.632-0.561 3.536-1.464 1.464-2.156 1.464-3.536c0-0.552-0.448-1-1-1s-1 0.448-1 1z" />
-            </svg>
-          </button>
-        </div>
-      </div>
-    </div>
+export function Autocomplete(
+  props: Partial<AutocompleteOptions<AutocompleteItem>>
+): JSX.Element {
+  const [autocompleteState, setAutocompleteState] = React.useState<
+    AutocompleteState<AutocompleteItem>
+  >({
+    collections: [],
+    completion: null,
+    context: {},
+    isOpen: false,
+    query: "",
+    activeItemId: null,
+    status: "idle",
+  });
+  const autocomplete = React.useMemo(
+    () =>
+      createAutocomplete<
+        AutocompleteItem,
+        React.BaseSyntheticEvent,
+        React.MouseEvent,
+        React.KeyboardEvent
+      >({
+        onStateChange({ state }) {
+          setAutocompleteState(state);
+        },
+        getSources() {
+          return [
+            {
+              sourceId: "products",
+              getItems({ query }) {
+                return getAlgoliaResults({
+                  searchClient,
+                  queries: [
+                    {
+                      indexName: "instant_search",
+                      query,
+                      params: {
+                        hitsPerPage: 5,
+                        highlightPreTag: "<mark>",
+                        highlightPostTag: "</mark>",
+                      },
+                    },
+                  ],
+                });
+              },
+              getItemUrl({ item }) {
+                return item.url;
+              },
+            },
+          ];
+        },
+        ...props,
+      }),
+    [props]
   );
-}
-
-export function Autocomplete(props: any) {
-  const containerRef = React.useRef(null);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const formRef = React.useRef<HTMLFormElement>(null);
+  const panelRef = React.useRef<HTMLDivElement>(null);
+  const { getEnvironmentProps } = autocomplete;
 
   React.useEffect(() => {
-    if (!containerRef.current) {
+    if (!formRef.current || !panelRef.current || !inputRef.current) {
       return undefined;
     }
 
-    const search = autocomplete({
-      container: containerRef.current,
-      placeholder: "Search for products",
-      renderer: {
-        createElement: React.createElement,
-        Fragment: React.Fragment,
-      },
-      render({ children }, root) {
-        render(children as any, root);
-      },
-      getSources({ query }) {
-        return [
-          {
-            sourceId: "products",
-            getItems() {
-              return getAlgoliaResults({
-                searchClient,
-                queries: [
-                  {
-                    indexName: "instant_search",
-                    query,
-                    params: {
-                      hitsPerPage: 5,
-                      attributesToSnippet: ["name:10", "description:35"],
-                      snippetEllipsisText: "…",
-                    },
-                  },
-                ],
-              });
-            },
-            templates: {
-              item({ item, components }) {
-                return <ProductItem hit={item} components={components} />;
-              },
-            },
-          },
-        ];
-      },
+    const { onTouchStart, onTouchMove } = getEnvironmentProps({
+      formElement: formRef.current,
+      inputElement: inputRef.current,
+      panelElement: panelRef.current,
     });
+
+    window.addEventListener("touchstart", onTouchStart);
+    window.addEventListener("touchmove", onTouchMove);
+
     return () => {
-      search.destroy();
+      window.removeEventListener("touchstart", onTouchStart);
+      window.removeEventListener("touchmove", onTouchMove);
     };
-  });
-  return <div ref={containerRef} style={{ marginTop: "2rem" }} />;
+  }, [getEnvironmentProps, formRef, inputRef, panelRef]);
+
+  return (
+    <div className="relative" {...autocomplete.getRootProps({})}>
+      <form
+        ref={formRef}
+        {...autocomplete.getFormProps({ inputElement: inputRef.current })}
+      >
+        <label className="sr-only" {...autocomplete.getLabelProps({})}>
+          Search
+        </label>
+        <div className="relative text-gray-400 focus-within:text-gray-600">
+          <div className="pointer-events-none absolute inset-y-0 left-0 px-3 flex items-center">
+            <button
+              type="submit"
+              title="Submit"
+              className={classNames(
+                "rounded-md pointer-events-auto p-1 -ml-1",
+                "focus:outline-none focus:ring-2 focus:ring-teal-600"
+              )}
+            >
+              <SearchIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </div>
+          <input
+            className="block w-full bg-white py-2 pl-11 pr-3 border border-transparent rounded-md leading-5 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-600 focus:ring-white focus:border-white sm:text-sm"
+            ref={inputRef}
+            {...autocomplete.getInputProps({
+              inputElement: inputRef.current,
+            })}
+          />
+        </div>
+      </form>
+
+      {autocompleteState.isOpen ? (
+        <div
+          ref={panelRef}
+          className={[
+            "aa-Panel",
+            "aa-Panel--desktop",
+            autocompleteState.status === "stalled" && "aa-Panel--stalled",
+          ]
+            .filter(Boolean)
+            .join(" ")}
+          {...autocomplete.getPanelProps({})}
+        >
+          <div
+            className={classNames(
+              "absolute inset-x-0 mt-4 overflow-hidden bg-white shadow",
+              "sm:mt-6 sm:w-96 sm:rounded-md",
+              // Full bleed form up until `sm`
+              "w-screen ml-[calc(50%-0.25rem)] transform -translate-x-1/2",
+              // Reset full bleed for `sm` and above
+              "sm:w-[calc(100%+8rem)] sm:-ml-16 sm:translate-x-0"
+            )}
+          >
+            {autocompleteState.collections.map((collection, index) => {
+              const { source, items } = collection;
+              return (
+                <section key={`source-${index}`}>
+                  {items.length > 0 && (
+                    <ul
+                      className="divide-y divide-gray-200"
+                      {...autocomplete.getListProps()}
+                    >
+                      {items.map((item, index) => {
+                        return (
+                          <li
+                            key={item.objectID}
+                            {...autocomplete.getItemProps({ item, source })}
+                          >
+                            <a
+                              href={item.url}
+                              className={classNames(
+                                index === 0 && "rounded-t-md",
+                                index === items.length - 1 && "rounded-b-md",
+                                "block border-2 border-transparent",
+                                "hover:bg-gray-50",
+                                "focus:outline-none focus:ring-inset focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-600 focus:ring-white focus:border-white"
+                              )}
+                            >
+                              <div className="flex items-center px-4 py-4 sm:px-6">
+                                <div className="flex items-center flex-1 min-w-0">
+                                  <div className="flex-shrink-0 border p-1 rounded">
+                                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                                    <img
+                                      className="w-12 h-12 object-contain"
+                                      src={item.image}
+                                      alt={item.name}
+                                    />
+                                  </div>
+                                  <div className="flex-1 min-w-0 px-4">
+                                    <div>
+                                      <p className="text-sm font-medium text-teal-700 truncate">
+                                        {item.name}
+                                      </p>
+                                      <div className="flex items-center mt-2 text-sm text-gray-500">
+                                        <p className="truncate">
+                                          By <strong>{item.brand}</strong> in{" "}
+                                          <strong>{item.categories[0]}</strong>
+                                        </p>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div>
+                                  <ChevronRightIcon
+                                    className="w-5 h-5 text-gray-400"
+                                    aria-hidden="true"
+                                  />
+                                </div>
+                              </div>
+                            </a>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  )}
+                </section>
+              );
+            })}
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
 }
